b644934fa30ff22f1e84c08f661562a10cfcc0d0
rr512 <rr512@DESKTOP-UV61PO2.mshome.net>
2023-08-24 01:08:22 +0530

    Initial Commit

--- loans/src/main/resources/data.sql
+++ loans/src/main/resources/data.sql
@@ -10,8 +10,8 @@ CREATE TABLE CUSTOMER (
 );
 
 CREATE TABLE LOANS (
-  loan_number INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT NOT NULL,
+  loan_number INT AUTO_INCREMENT PRIMARY KEY,
   loan_type VARCHAR(50) NOT NULL,
   loan_amount INT NOT NULL,
   loan_end_date DATE DEFAULT CURDATE() + 1 DAY
@@ -20,20 +20,20 @@ CREATE TABLE LOANS (
 
 
 INSERT INTO CUSTOMER (customer_id, name, mobile_number, email_id, created_date)
-VALUES (1, 'sourav', '9876543210', 'sourav@test.com', CURDATE());
+VALUES ('1', 'sourav', '9876543210', 'sourav@test.com', CURDATE());
 
 
 
 
 
-INSERT INTO LOANS (customer_id, loan_number, loan_type, loan_amount, loan_end_date)
-VALUES (01, 1, 'HOUSING LOAN', 10000, CURDATE()+1000);
+INSERT INTO LOANS ( loan_number, loan_type, loan_amount, loan_end_date)
+VALUES ( '1', 'HOUSING LOAN', '10000', CURDATE()+1000);
 
-INSERT INTO LOANS (customer_id, loan_number, loan_type, loan_amount, loan_end_date)
-VALUES (02, 2, 'VEHICLE LOAN', 50000, CURDATE()+100);
+INSERT INTO LOANS ( loan_number, loan_type, loan_amount, loan_end_date)
+VALUES ( '2', 'VEHICLE LOAN', '50000', CURDATE()+100);
 
-INSERT INTO LOANS (customer_id, loan_number, loan_type, loan_amount, loan_end_date)
-VALUES (03, 3, 'HOUSING LOAN', 80000, CURDATE()+5000);
+INSERT INTO LOANS ( loan_number, loan_type, loan_amount, loan_end_date)
+VALUES ( '3', 'HOUSING LOAN', '80000', CURDATE()+5000);
 
-INSERT INTO LOANS (customer_id, loan_number, loan_type, loan_amount, loan_end_date)
-VALUES (04, 4, 'PERSONAL LOAN', 7000, CURDATE()+10);
+INSERT INTO LOANS ( loan_number, loan_type, loan_amount, loan_end_date)
+VALUES ( '4', 'PERSONAL LOAN', '7000', CURDATE()+10);
